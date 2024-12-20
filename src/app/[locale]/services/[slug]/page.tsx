27a5f0357424b0b7687f98c3f0a6bb5fcf55d870
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2024-12-20 13:37:34 +0300

    added generatemeta date and edited json

--- src/app/[locale]/services/[slug]/page.tsx
+++ src/app/[locale]/services/[slug]/page.tsx
@@ -4,7 +4,7 @@ import { Metadata } from 'next'
 import { notFound } from 'next/navigation'
 import { Zap, Shield, Paintbrush, AppWindowIcon as Window, Maximize, Layers, Blinds, ThumbsUp, Lock, Clock, Smile, Sun, Leaf, Thermometer, Heart, Glasses, Clipboard, CheckCircle, Hammer, SwatchBook, VolumeOff, ShieldCheck, Quote, Phone, ArrowRight } from 'lucide-react'
 import { useTranslations } from 'next-intl'
-import { setRequestLocale } from 'next-intl/server'
+import { getTranslations, setRequestLocale } from 'next-intl/server'
 import PageLayout from '@/components/PageLayout'
 import ReactPlayer from 'react-player'
 import { Card } from '@/components/ui/card'
@@ -34,71 +34,116 @@ const iconMap = {
   hammer: Hammer,
   volumeOff: VolumeOff
 }
+
+
 type Props = {
-  params: { locale: string, slug: string }; 
+  params: { locale: string; slug: string }
 }
 
+type Service = {
+  title: string
+  slug: string
+  metaTitle?: string
+  metaDescription?: string
+  keywords?: string
+  introduction?: string
+  description: string
+  description2: string
+  image: string
+  video?: string
+  pricingTitle: string
+  pricingDescription: string
+  advantages?: Array<{ icon: string; title: string; description: string }>
+  galleryImages?: Array<{ name: string; image: string }>
+}
 
-export default function ServicePage({ params: { locale, slug } }: Props) {
-  
-  // Locale'i ayarla
-  setRequestLocale(locale);
-  const t = useTranslations('services')
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { locale, slug } = params
 
-  // Hizmetler listesi
-  const servicesList = t.raw('list') as Array<{
-    title: string
-    description: string
-    description2: string
-    image: string
-    video?: string,
-    id: string
-    slug: string  // Slug'ı da ekliyoruz
-    metaTitle?: string
-    metaDescription?: string
-    introduction?: string
-    pricingTitle: string,
-    pricingDescription: string,
-    heroImage?: string
-    advantages?: Array<{ icon: string; title: string; description: string }>
-    galleryImages?: Array<{ name: string; image: string }>
-  }>
+  // Use getTranslations instead of useTranslations for server components
+  const t = await getTranslations('services')
 
+  // Fetch the services list
+  const servicesList = t.raw('list') as Array<Service>
 
-  // Locale ve slug'a göre hizmeti bul
+  // Find the service based on the slug
   const service = servicesList.find(service => service.slug === slug)
 
   if (!service) {
-    notFound() // Eğer hizmet bulunmazsa 404 sayfasına yönlendir
+    return {
+      title: 'Service Not Found',
+      description: 'The requested service could not be found.',
+    }
   }
-  const [highlightedWord] = service.introduction?.split(" ") || [];
- 
+
+  return {
+    title: service.metaTitle || service.title,
+    description: service.metaDescription || service.description,
+    keywords: service.keywords,
+    openGraph: {
+      title: service.metaTitle || service.title,
+      description: service.metaDescription || service.description,
+      images: [
+        {
+          url: service.image,
+          width: 1200,
+          height: 630,
+          alt: service.title,
+        },
+      ],
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: service.metaTitle || service.title,
+      description: service.metaDescription || service.description,
+      images: [service.image],
+    },
+  }
+}
+
+export default function ServicePage({ params: { locale, slug } }: Props) {
+  const t = useTranslations('services')
+
+  // Fetch the services list
+  const servicesList = t.raw('list') as Array<Service>
+
+  // Find the service based on the slug
+  const service = servicesList.find(service => service.slug === slug)
+
+  if (!service) {
+    notFound()
+  }
+
+  const [highlightedWord] = service.introduction?.split(" ") || []
+
   return (
-    <PageLayout title={service?.title} image={service?.image}>
-      <div className=" mx-auto ">
+    <PageLayout title={service.title} image={service.image}>
+      <div className="mx-auto">
         {/* Introduction */}
-        <section className="bg-white py-16 dark:bg-gray-900  ">
+        <section className="bg-white py-16 dark:bg-gray-900">
           <div className="container mx-auto px-4 md:px-6 lg:px-8 mt-5 mb-5">
-          <AnimatedHeading text={service.introduction} highlightedWord={highlightedWord}/>
+            <AnimatedHeading text={service.introduction || ''} highlightedWord={highlightedWord} />
             <hr className="my-6 border-t-2 border-[#ff0505d9] max-w-[150px] mx-auto" />
-            <p className="max-w-6xl text-xl md:text-2xl  mx-auto text-center text-gray-600  dark:text-white ">
+            <p className="max-w-6xl text-xl md:text-2xl mx-auto text-center text-gray-600 dark:text-white">
               {service.description2}
             </p>
           </div>
         </section>
-        {/* Pricing Section */}
-        <section className="flex flex-col md:flex-row h-auto md:h-[540px] ">
-          {/* Video Section */}
+
+        {/* Video Section */}
+        <section className="flex flex-col md:flex-row h-auto md:h-[540px]">
           <div className="relative w-full md:w-1/2 h-[200px] md:h-full">
-            <video
-              src={service?.video}
-              autoPlay
-              loop
-              muted
-              className="w-full h-full object-cover"
-            >
-              {t('videoError')}
-            </video>
+            {service.video && (
+              <video
+                src={service.video}
+                autoPlay
+                loop
+                muted
+                className="w-full h-full object-cover"
+              >
+                {t('videoError')}
+              </video>
+            )}
             <div className="absolute inset-0 bg-gradient-to-r from-gray-600 via-gray-900/70 to-transparent dark:bg-gradient-to-r dark:from-gray-900 dark:via-gray-900/70 dark:to-transparent" />
           </div>
 
@@ -119,22 +164,23 @@ export default function ServicePage({ params: { locale, slug } }: Props) {
             </div>
           </div>
         </section>
+
         {/* Projects */}
-        {service.galleryImages && service.galleryImages.length > 0 ? (
+        {service.galleryImages && service.galleryImages.length > 0 && (
           <section className="mx-auto px-4 py-8 md:py-20 dark:bg-gray-800 mb-8 bg-white">
-            <h2 className="text-4xl font-semibold mb-2 group-hover:text-blue-600 transition-colors duration-300 dark:text-white ">
-              {service?.title} {t('projects')}
+            <h2 className="text-4xl font-semibold mb-2 group-hover:text-blue-600 transition-colors duration-300 dark:text-white">
+              {service.title} {t('projects')}
             </h2>
             <hr className="my-6 border-t-2 border-[#ff0505d9] max-w-[150px]" />
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-              {service.galleryImages.map((images, index) => (
+              {service.galleryImages.map((image, index) => (
                 <div
                   key={index}
                   className="bg-white dark:bg-gray-900/70 rounded-lg shadow-md overflow-hidden transition-transform duration-300 ease-in-out transform hover:-translate-y-2 mt-5"
                 >
                   <div className="relative h-64">
                     <Image
-                      src={images.image}
+                      src={image.image}
                       alt={`${service.title} project ${index + 1}`}
                       fill
                       className="object-cover"
@@ -142,16 +188,15 @@ export default function ServicePage({ params: { locale, slug } }: Props) {
                   </div>
                   <div className="p-4">
                     <h2 className="text-lg font-semibold mb-2 group-hover:text-blue-600 transition-colors duration-300 dark:text-white">
-                      {images.name}
+                      {image.name}
                     </h2>
                   </div>
                 </div>
               ))}
             </div>
           </section>
-        ) : (
-          <p className="text-gray-500 col-span-full text-center">{' '}</p>
         )}
+
         {/* Advantages Section */}
         <section className="bg-gray-100 py-10 md:py-10 dark:bg-gray-900">
           <div className="container mx-auto px-4 md:px-6 lg:px-8">
@@ -159,10 +204,10 @@ export default function ServicePage({ params: { locale, slug } }: Props) {
               {t('advantagesTitle')}
             </h2>
             <div className="w-24 h-1 bg-[#ff0505d9] mb-6 mx-auto"></div>
-            <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4  ">
+            <div className="grid gap-8 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4">
               {service.advantages &&
                 service.advantages.map((advantage, index) => {
-                  const Icon = iconMap[advantage.icon as keyof typeof iconMap];
+                  const Icon = iconMap[advantage.icon as keyof typeof iconMap] || Zap;
                   return (
                     <Card
                       key={index}
@@ -183,9 +228,8 @@ export default function ServicePage({ params: { locale, slug } }: Props) {
             </div>
           </div>
         </section>
-       
-        <MapSection/>
-        
+
+        <MapSection />
       </div>
     </PageLayout>
   )
