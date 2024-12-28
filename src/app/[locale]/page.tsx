894c9a2aaaa3712ee19e8afbccdbd0898769bed1
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2024-12-28 10:32:09 +0300

    page tsx error fixed

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,5 +1,5 @@
-
-import { getTranslations, setRequestLocale } from 'next-intl/server';
+import type { Metadata, ResolvingMetadata } from 'next'
+import { getTranslations } from 'next-intl/server';
 import Hero from '@/components/Hero';
 import { Services } from '@/components/Services';
 import { Franchises } from '@/components/Franchises';
@@ -7,28 +7,32 @@ import PartnersSection from '@/components/PartnersSection';
 import { MapSection } from '@/components/MapSection';
 
 type Props = {
-  params: { locale: string };
-};
+  params: Promise<{ locale: string }>
+}
 
-export async function generateMetadata({
-  params: { locale }
-}: Omit<Props, 'children'>) {
+export async function generateMetadata(
+  { params }: Props,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { locale } = await params;
   const t = await getTranslations({ locale, namespace: 'indexPage' });
+
+  // Optionally access and extend (rather than replace) parent metadata
+  const previousKeywords = (await parent).keywords || [];
 
   return {
     title: t('metaTitle'),
     description: t('metaDescription'),
-    keywords:t('keywords')
+    keywords: [...(t('keywords').split(',').map(keyword => keyword.trim())), ...previousKeywords],
   };
 }
-export default function IndexPage({ params: { locale } }: Props) {
-  // Enable static rendering
-  setRequestLocale(locale);
 
-
+export default async function IndexPage({ params }: Props) {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: 'indexPage' });
 
   return (
-    <main className="min-h-screen bg-gray-900 ">
+    <main className="min-h-screen bg-gray-900">
       <Hero />
       <Franchises />
       <Services />
@@ -37,3 +41,4 @@ export default function IndexPage({ params: { locale } }: Props) {
     </main>
   );
 }
+
