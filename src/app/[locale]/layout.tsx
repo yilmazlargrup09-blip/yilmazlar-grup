503758548bccaf84b089d26f011b36961125b417
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2024-12-28 10:30:05 +0300

    locallayout error fixed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -3,30 +3,38 @@ import { getTranslations, setRequestLocale } from 'next-intl/server';
 import { ReactNode } from 'react';
 import BaseLayout from '@/components/BaseLayout';
 import { routing, Locale } from '@/i18n/routing';
+import type { Metadata, ResolvingMetadata } from 'next'
 
 type Props = {
   children: ReactNode;
-  params: { locale: string };
+  params: Promise<{ locale: string }>;
 };
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
 }
 
-export async function generateMetadata({
-  params: { locale }
-}: Omit<Props, 'children'>) {
+export async function generateMetadata(
+  { params }: Omit<Props, 'children'>,
+  parent: ResolvingMetadata
+): Promise<Metadata> {
+  const { locale } = await params;
   const t = await getTranslations({ locale, namespace: 'LocaleLayout' });
 
+  // Optionally access and extend (rather than replace) parent metadata
+  const previousTitle = (await parent).title || '';
+
   return {
-    title: t('title'),
+    title: `${t('title')} | ${previousTitle}`,
   };
 }
 
 export default async function LocaleLayout({
   children,
-  params: { locale }
-}: Props) {
+  params
+}: Omit<Props, 'searchParams'>) {
+  const { locale } = await params;
+
   // Ensure that the incoming `locale` is valid
   if (!routing.locales.includes(locale as Locale)) {
     notFound();
