3e946d08a2b892c3b2fafd534c7c2e6402c919cb
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2024-12-28 09:54:11 +0300

    error

--- src/app/[locale]/about/page.tsx
+++ src/app/[locale]/about/page.tsx
@@ -3,36 +3,30 @@ import PageLayout from '@/components/PageLayout';
 import { useTranslations } from 'next-intl';
 import { getTranslations, setRequestLocale } from 'next-intl/server';
 
-type Params = {
-  locale: string;
-};
-
 type Props = {
-  params: Params; // Artık Promise değil, doğrudan nesne.
+  params: { locale: string };
 };
+// export async function generateMetadata({
+//   params: { locale }
+// }: Omit<Props, 'children'>) {
+//   const t = await getTranslations({ locale, namespace: 'about' });
 
-export async function generateMetadata({
-  params: { locale },
-}: Omit<Props, 'children'>) {
-  const t = await getTranslations({ locale, namespace: 'about' });
-
-  return {
-    title: t('metaTitle'),
-    description: t('metaDescription'),
-    keywords: t('keywords'),
-  };
-}
-
+//   return {
+//     title: t('metaTitle'),  
+//     description: t('metaDescription'),
+//     keywords:t('keywords')
+//   };
+// }
 export default function AboutPage({ params: { locale } }: Props) {
-  // Set locale for server-side rendering
+  // Enable static rendering
   setRequestLocale(locale);
 
-  // useTranslations can be used directly here because AboutPage is not async
   const t = useTranslations('about');
 
   return (
     <PageLayout title={t('title')} image={t('mainImage')}>
       <About />
     </PageLayout>
+
   );
 }
