982f48399fd24b832e606efbbec6a5bc5bb2a636
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2024-12-31 19:30:46 +0300

    error fixed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,52 +1,57 @@
-
+import { notFound } from 'next/navigation';
 import { getMessages, getTranslations, setRequestLocale } from 'next-intl/server';
-import {  Suspense } from 'react';
+import { ReactNode, Suspense } from 'react';
 import BaseLayout from '@/components/BaseLayout';
 import ClientWrapper from '@/components/ClientWrapper';
 import { routing, Locale } from '@/i18n/routing';
 import type { Metadata, ResolvingMetadata } from 'next';
 import LoadingScreen from '@/components/LoadingScreen';
-import GlobalNotFound from '../not-found';
 
 type Props = {
-  children: React.ReactNode;
-  params: {
-    locale: string;
-  };
+  children: ReactNode;
+  params: { locale: string };
 };
 
+// `generateStaticParams` fonksiyonu ile dinamik olarak tüm diller için parametreler oluşturuluyor.
+export function generateStaticParams() {
+  return routing.locales.map((locale) => ({ locale }));
+}
+
+// Sayfa metadata'sını dinamik olarak oluşturmak
 export async function generateMetadata(
-  { params }: { params: { locale: string } },
+  { params }: Props,
   parent: ResolvingMetadata
 ): Promise<Metadata> {
   const { locale } = params;
-  const t = await getTranslations({ locale, namespace: 'Layout' });
+  const t = await getTranslations({ locale, namespace: 'indexPage' });
 
-  // Optionally access and extend (rather than replace) parent metadata
+  // Varsayılan metadata'ya eklemeler yapmak
   const previousKeywords = (await parent).keywords || [];
 
   return {
     title: t('metaTitle'),
     description: t('metaDescription'),
-    keywords: [...(t('keywords', { defaultValue: '' }).split(',').map(keyword => keyword.trim())), ...previousKeywords],
+    keywords: [...(t('keywords').split(',').map(keyword => keyword.trim())), ...previousKeywords],
   };
 }
 
 export default async function LocaleLayout({
   children,
-  params,
+  params
 }: Props) {
-  const { locale } = await params; 
+  const { locale } = params; // No need for await here
 
+  // `locale`'ün geçerli olduğundan emin olunuyor
   if (!routing.locales.includes(locale as Locale)) {
-    return <GlobalNotFound />;
+    notFound();  // Geçersiz locale ile karşılaşıldığında sayfa bulunamadı
   }
 
-  const messages = await getMessages();
-  setRequestLocale(locale as Locale);
+  const messages = await getMessages(); // Sunucu tarafında mesajlar alınıyor
+  setRequestLocale(locale as Locale); // Locale ayarları yapılır
+  await new Promise(resolve => setTimeout(resolve, 1000))
   await new Promise(resolve => setTimeout(resolve, 1000));
-
   return (
+    // `Suspense` burada async bileşenler yüklenene kadar bekler
     <Suspense fallback={<LoadingScreen />}>
       <ClientWrapper locale={locale as Locale} messages={messages}>
         <BaseLayout locale={locale as Locale}>
