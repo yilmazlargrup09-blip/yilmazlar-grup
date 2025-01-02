00404e0f22447dce9da91fe03281f79a4eb0d799
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2025-01-02 19:15:52 +0300

    error fixed

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,17 +1,24 @@
 import { notFound } from 'next/navigation';
-import { getTranslations, setRequestLocale } from 'next-intl/server';
+import { getMessages, getTranslations, setRequestLocale } from 'next-intl/server';
 import { ReactNode, Suspense } from 'react';
 import BaseLayout from '@/components/BaseLayout';
 import { routing, Locale } from '@/i18n/routing';
 import type { Metadata, ResolvingMetadata } from 'next';
 import LoadingScreen from '@/components/LoadingScreen';
-import ClientWrapper from '@/components/ClientWrapper';
+import { NextIntlClientProvider } from 'next-intl';
+import { Poppins } from 'next/font/google';
 
 type Props = {
   children: ReactNode;
   params: Promise<{ locale: string }>;
-  
+
 };
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '600', '700'],
+  variable: '--font-poppins',
+  display: 'swap',
+});
 
 export function generateStaticParams() {
   return routing.locales.map((locale) => ({ locale }));
@@ -44,15 +51,23 @@ export default async function LocaleLayout({
   }
   setRequestLocale(locale as Locale);
   await new Promise(resolve => setTimeout(resolve, 1000));
-
+  const messages = await getMessages();
   return (
-    <Suspense fallback={<LoadingScreen />}>
-    <ClientWrapper locale={locale as Locale}> {/* Wrap the children with ClientWrapper */}
-      <BaseLayout locale={locale as Locale}>
-        {children}
-      </BaseLayout>
-    </ClientWrapper>
-  </Suspense>
+    <html lang={locale} suppressHydrationWarning>
+      <head>
+        <meta name="google-site-verification" content="googleae1ab6bc1b2473b9.html" />
+      </head>
+      <body className={`${poppins.variable} font-sans`} suppressHydrationWarning>
+        <NextIntlClientProvider locale={locale} messages={messages}>
+          <Suspense fallback={<LoadingScreen />}>
+            <BaseLayout locale={locale as Locale}>
+              {children}
+            </BaseLayout>
+          </Suspense>
+        </NextIntlClientProvider>
+      </body>
+    </html>
+
   );
 }
 
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,35 +1,12 @@
 import { ReactNode } from 'react';
 import './globals.css'
-import { Poppins } from 'next/font/google';
-import { NextIntlClientProvider } from 'next-intl';
-import { getMessages } from 'next-intl/server';
-
-const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['300', '400', '600', '700'],
-  variable: '--font-poppins',
-  display: 'swap',
-});
-
 
 type Props = {
   children: ReactNode;
-  locale: string;
 };
 
-export default async function RootLayout({ children,locale }: Props) {
-  const messages = await getMessages();
-  return (
-    <html lang={locale} suppressHydrationWarning>
-      <head>
-        <meta name="google-site-verification" content="googleae1ab6bc1b2473b9.html" />
-      </head>
-      <body className={`${poppins.variable} font-sans`} suppressHydrationWarning>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          {children}
-        </NextIntlClientProvider>
-      </body>
-    </html>
-  );
+export default async function RootLayout({ children}: Props) {
+
+  return ( <>{children}</> );
 }
 
