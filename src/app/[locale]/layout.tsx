a0cd0ab1eb36338b702b39cab4b8982e7c5f6a68
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2025-01-03 16:18:55 +0300

    Update layout.tsx

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,5 +1,5 @@
 import { notFound } from 'next/navigation';
-import { getTranslations } from 'next-intl/server';
+import { getTranslations, unstable_setRequestLocale } from 'next-intl/server';
 import { ReactNode } from 'react';
 import BaseLayout from '@/components/BaseLayout';
 import { routing, Locale } from '@/i18n/routing';
@@ -15,8 +15,9 @@ export function generateStaticParams() {
 }
 
 export async function generateMetadata(
-  { params: { locale } }: Props
+  { params }: Props
 ): Promise<Metadata> {
+  const locale = params.locale;
   const t = await getTranslations({ locale, namespace: 'LocaleLayout' });
 
   return {
@@ -24,9 +25,15 @@ export async function generateMetadata(
   };
 }
 
-export default async function LocaleLayout({ children, params: { locale } }: Props) {
+export default async function LocaleLayout({
+  children,
+  params: { locale }
+}: Props) {
   // Validate that the incoming `locale` parameter is valid
   if (!routing.locales.includes(locale as Locale)) notFound();
+
+  // Enable static rendering
+  unstable_setRequestLocale(locale);
 
   return (
     <BaseLayout locale={locale}>
