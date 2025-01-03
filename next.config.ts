f054d1f7b7545eb925bfc677e84f53533869e7c0
Aleyna Özçelik <97678679+Aleynaozc@users.noreply.github.com>
2025-01-03 15:45:59 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,11 +3,6 @@
 const withNextIntl = require('next-intl/plugin')();
 
 /** @type {import('next').NextConfig} */
-const config = {
-  i18n: {
-    locales: ['en', 'tr', 'ru'],  // Desteklediğiniz diller burada
-    defaultLocale: 'tr',          // Varsayılan dil Türkçe
-  },
-};
+const config = {};
 
 module.exports = withNextIntl(config);
